f1075fd73e655bd3bbff1ba7b6b96e9c4c527578
Joltkevitch <=>
2023-05-12 21:38:48 +0200

    feat(init): init version of the program main function
    app compiles and makes a changelog with the git history.

--- console-dummy/Program.cs
+++ console-dummy/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace console_dummy
+{
+
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Changelog changelog = Changelog.Instance;
+            string gitHistory = CommandDiagnostics.OutputCommand("git", "log");
+            string lastSemVerVersion = CommandDiagnostics.OutputCommand("git", "tag -l").Split("\n")[0];
+            string changelogPath = "./changelog.md";
+            changelog.ExtractCommitsFromString(gitHistory);
+            changelog.SetCurrentVersion(lastSemVerVersion);
+            changelog.ParseCommitForChangeLog();
+            changelog.WriteToChangelog(changelogPath);
+        }
+    }
+}
